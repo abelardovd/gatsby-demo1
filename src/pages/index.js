02513e3b03cd856dee17b8b1a1a5527a85a64724
abelardo <ivo_v97@hotmail.com>
2018-09-25 21:55:31 -0500

    change 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import styled from 'styled-components'
 import Layout from '../components/layout'
+
+import icon from '../images/gatsby-icon.png'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const Image = styled.img`
+  height: 100px;
+
+`
+
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <Title>Hi people I am Abelardo</Title>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Image src={icon} alt='something here'/>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
