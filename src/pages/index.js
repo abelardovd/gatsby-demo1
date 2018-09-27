cfe93f05a6c7040d9c635aa88f18ace10df306a3
abelardo <ivo_v97@hotmail.com>
2018-09-27 18:55:41 -0500

    bb

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import logo from '../images/bbcutie-logo.png'
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color: palevioletred;
+  color: black;
 `
 
 const Image = styled.img`
