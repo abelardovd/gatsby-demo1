f4ac98171b760553a02764f7c732fdc0f21aa7ca
abelardo <ivo_v97@hotmail.com>
2018-09-27 20:48:35 -0500

    bb

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
+
+const Button = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  justifyContent: center;
+  alignItems: center;
+`
 
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
+    <Button
+      href="https://github.com/styled-components/styled-components"
+      target="_blank"
+      rel="noopener"
+      primary
+    >
+      GitHub
+    </Button>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
