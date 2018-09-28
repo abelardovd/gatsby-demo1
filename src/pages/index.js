36c4a453168c63122d55b1a8bc1c5a43cd282cb3
abelardo <ivo_v97@hotmail.com>
2018-09-27 19:36:38 -0500

    bb

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,8 @@ const IndexPage = () => (
     <Title>BOLD FLAVORS. SERIOUSLY SMOKED MEATS.™</Title>
     <Par1>For over 65 years of great food, friendly faces, and quick casual service, Bar-B-Cutie® SmokeHouse is second to none! We pride ourselves on the quality of our cooking, the contentment of our guests, and the continuity of our business. </Par1>
     <Par1>So when you have that familiar craving for a mouth-waterin’ meal, you need to Get Your Booty To Bar-B-Cutie!</Par1>
-    <Button>Normal Button</Button>
+    <Button>Get to Know Us</Button> <Button>Read Our Story</Button>
+    
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
