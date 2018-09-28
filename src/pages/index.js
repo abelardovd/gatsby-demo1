8f9e38baa943432971afb4a52fdf5a3c9bbac41e
abelardo <ivo_v97@hotmail.com>
2018-09-27 21:47:15 -0500

    Final bar-b-cutie

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,20 +18,29 @@ const Par1 = styled.h1`
 `
 
 const Image = styled.img`
-  
-  align: center;
-  height: 300px;
+display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
 `
-const Button = styled.button`
-  display: inline-block;
-  color: palevioletred;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-  justifyContent: center;
-  alignItems: center;
+const Button = styled.a`
+
+display: inline-block;
+    position: relative;
+    color: rgb(17, 17, 17);
+    font-weight: bold;
+    overflow: hidden;
+    text-decoration: none;
+    padding: 10px 15px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(170, 170, 170);
+    border-image: initial;
+    :hover {
+      color: red;
+    }
+    
+    
 `
 
 
@@ -41,7 +50,9 @@ const IndexPage = () => (
     <Title>BOLD FLAVORS. SERIOUSLY SMOKED MEATS.™</Title>
     <Par1>For over 65 years of great food, friendly faces, and quick casual service, Bar-B-Cutie® SmokeHouse is second to none! We pride ourselves on the quality of our cooking, the contentment of our guests, and the continuity of our business. </Par1>
     <Par1>So when you have that familiar craving for a mouth-waterin’ meal, you need to Get Your Booty To Bar-B-Cutie!</Par1>
-    <Button>Get To Know Us</Button> <Button>Read Our Story</Button>
+    <Button href="https://www.bar-b-cutie.com">Get To Know Us</Button> 
+    <Button href="https://www.bar-b-cutie.com/bbq/history/" >Read Our Story</Button>
+    
     
     
     
